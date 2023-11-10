81f4a808c88a74650e3751d56c6fba7eb607d8dd
Leonard Chiu <leonardchiu@Leonards-Air-2.localdomain>
2023-11-11 01:03:40 +0800

    revert to reference env file

--- src/lib/strapi.ts
+++ src/lib/strapi.ts
@@ -24,8 +24,8 @@ export default async function fetchApi<T>({
     endpoint = endpoint.slice(1);
   }
   // const url = import.meta.env.PUBLIC_STRAPI_URL + "/api/" + endpoint;
-  // const url = new URL(`${import.meta.env.PUBLIC_STRAPI_URL}/api/${endpoint}`);
-  const url = new URL(`http://strapi.leonardchiu.com:1337/api/${endpoint}`);
+  const url = new URL(`${import.meta.env.PUBLIC_STRAPI_URL}/api/${endpoint}`);
+  // const url = new URL(`http://strapi.leonardchiu.com:1337/api/${endpoint}`);
 
   if (query) {
     Object.entries(query).forEach(([key, value]) => {
@@ -34,9 +34,9 @@ export default async function fetchApi<T>({
   }
   const res = await fetch(url.toString(), {
     headers: {
-      // Authorization: `Bearer ${import.meta.env.PUBLIC_apiKEY}`,
-      Authorization:
-        "Bearer a14ce8824cfd6c2e52bfaf8e38501bf20d865f190df240e17eca264bd228e1a2e85b1cbe2e8add492652ec7e19afc70d233c6b5966cee10663b382087fdca4e597d7d07842845cc1a0b483faa356337fd8b6425ca557087ca6471fbb0b48ec19174e92c7b86e84df5b87520e3d34e3ec8f57c55cdaba0663dc2939ca90a5fd1e",
+      Authorization: `Bearer ${import.meta.env.PUBLIC_apiKEY}`,
+      // Authorization:
+      //   "Bearer a14ce8824cfd6c2e52bfaf8e38501bf20d865f190df240e17eca264bd228e1a2e85b1cbe2e8add492652ec7e19afc70d233c6b5966cee10663b382087fdca4e597d7d07842845cc1a0b483faa356337fd8b6425ca557087ca6471fbb0b48ec19174e92c7b86e84df5b87520e3d34e3ec8f57c55cdaba0663dc2939ca90a5fd1e",
     },
   });
   let data = await res.json();
